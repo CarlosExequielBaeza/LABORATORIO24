f3155703e2b817daf29a33a3b95f90a3bfe5aa66
Carlos Baeza <132070988+CarlosExequielBaeza@users.noreply.github.com>
2024-08-29 19:04:45 -0300

    Reloj

--- PRACTICO 1/TP2Reloj/src/main/java/com/mycompany/tp2reloj/TP2Reloj.java
+++ PRACTICO 1/TP2Reloj/src/main/java/com/mycompany/tp2reloj/TP2Reloj.java
@@ -2,13 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
 
-package com.mycompany.tp3laboratorio;
+package com.mycompany.tp2reloj;
 
 /**
  *
  * @author litob
  */
-public class TP3Laboratorio {
+public class TP2Reloj {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
--- PRACTICO 1/TP3Laboratorio/src/main/java/TP3Laboratorio/vistas/NewJFrame.java
+++ PRACTICO 1/TP3Laboratorio/src/main/java/TP3Laboratorio/vistas/NewJFrame.java
@@ -2,18 +2,18 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
-package com.mycompany.tp3laboratorio;
+package TP3Laboratorio.vistas;
 
 /**
  *
  * @author litob
  */
-public class Ejercicio1 extends javax.swing.JFrame {
+public class NewJFrame extends javax.swing.JFrame {
 
     /**
-     * Creates new form Ejercicio1
+     * Creates new form NewJFrame
      */
-    public Ejercicio1() {
+    public NewJFrame() {
         initComponents();
     }
 
@@ -59,20 +59,20 @@ public class Ejercicio1 extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Ejercicio1.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(NewJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(Ejercicio1.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(NewJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(Ejercicio1.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(NewJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(Ejercicio1.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(NewJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new Ejercicio1().setVisible(true);
+                new NewJFrame().setVisible(true);
             }
         });
     }
