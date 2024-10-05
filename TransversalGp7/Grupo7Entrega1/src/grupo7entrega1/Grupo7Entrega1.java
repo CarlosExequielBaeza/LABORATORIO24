93f16145623bf0fe5ec573fbe22cc9b0b71d0563
Carlos Baeza <132070988+CarlosExequielBaeza@users.noreply.github.com>
2024-10-05 11:02:13 -0300

    uppp

---
+++ TransversalGp7/Grupo7Entrega1/src/grupo7entrega1/Grupo7Entrega1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package grupo7entrega1;
+
+/**
+ *
+ * @author litob
+ */
+public class Grupo7Entrega1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
